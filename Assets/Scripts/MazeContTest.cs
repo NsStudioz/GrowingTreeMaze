9d12b2d61af36fb0ce6de1e982950d103c5bd82a
NsStudioz <sneve2016@gmail.com>
2023-10-26 13:02:33 +0300

    refactor: MazeGrid.cs = set it to derive from monobehaviour & added maze grid generation functionality.

--- Assets/Scripts/MazeContTest.cs
+++ Assets/Scripts/MazeContTest.cs
@@ -4,46 +4,17 @@ using UnityEngine;
 
 public class MazeContTest : MonoBehaviour
 {
-    public Node nodePrefab;
-
-    [SerializeField] private int gridWidth;
-    [SerializeField] private int gridHeight;
 
     void Start()
     {
-        InitializeMazeGrid();
-        DrawMazeGrid();
+        //InitializeMazeGridSize();
     }
 
-    private void InitializeMazeGrid()
+/*    private void InitializeMazeGridSize()
     {
         MazeGrid mazeGrid = new MazeGrid(gridWidth, gridHeight);
-    }
+    }*/
 
-    private void DrawMazeGrid()
-    {
-        for (int x = 0; x < gridWidth; x++)
-            for (int z = 0; z < gridHeight; z++)
-            {
-                Node newNode = SpawnNewCellInstance(nodePrefab, SetNodePosition(x, z));
-                SetNodeAsChildToParent(newNode, transform);
-            }
-    }
 
-    private Node SpawnNewCellInstance(Node prefab, Vector3 cellPos)
-    {
-        return Instantiate(prefab, cellPos, Quaternion.identity);
-    }
-
-    private Vector3 SetNodePosition(int x, int z)
-    {
-        Vector3 nodePos = new Vector3(x, 0, z);
-        return nodePos;
-    }
-
-    private void SetNodeAsChildToParent(Node prefab, Transform parent)
-    {
-        prefab.transform.parent = parent;
-    }
 
 }
--- Assets/Scripts/MazeGrid.cs
+++ Assets/Scripts/MazeGrid.cs
@@ -2,15 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MazeGrid
+public class MazeGrid : MonoBehaviour
 {
-    private int width;
-    private int height;
+    /*    private int width;
+        private int height;
 
-    public MazeGrid(int width, int height)
+        public MazeGrid(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }*/
+
+    [SerializeField] private Node nodePrefab;
+
+    [SerializeField] private int gridWidth;
+    [SerializeField] private int gridHeight;
+
+    private void Start()
     {
-        this.width = width;
-        this.height = height;
+        DrawMazeGrid();
     }
 
+    private void DrawMazeGrid()
+    {
+        for (int x = 0; x < gridWidth; x++)
+            for (int z = 0; z < gridHeight; z++)
+            {
+                Node newNode = SpawnNewCellInstance(nodePrefab, SetNodePosition(x, z));
+                SetNodeAsChildToParent(newNode, transform);
+            }
+    }
+
+    private Node SpawnNewCellInstance(Node prefab, Vector3 cellPos)
+    {
+        return Instantiate(prefab, cellPos, Quaternion.identity);
+    }
+
+    private Vector3 SetNodePosition(int x, int z)
+    {
+        Vector3 nodePos = new Vector3(x, 0, z);
+        return nodePos;
+    }
+
+    private void SetNodeAsChildToParent(Node prefab, Transform parent)
+    {
+        prefab.transform.parent = parent;
+    }
 }
