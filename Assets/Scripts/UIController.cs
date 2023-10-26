dfd1d30f73ca7bedc8e1d5f49b645fa41e7816ff
NsStudioz <sneve2016@gmail.com>
2023-10-26 15:38:37 +0300

    new: UIController.cs = new fields (UI elements).

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Main Elements")]
+    [SerializeField] private GameObject GamePanel;
+
+    [Header("Maze Value Texts")]
+    [SerializeField] private TMP_Text widthValueText;
+    [SerializeField] private TMP_Text heightValueText;
+
+    [Header("Sliders")]
+    [SerializeField] private Slider widthSilder;
+    [SerializeField] private Slider heightSilder;
+
+    [Header("Buttons")]
+    [SerializeField] private Button generateMazeBtn;
+    [SerializeField] private Button backBtn;
+
+    [Header("Toggle")]
+    [SerializeField] private Toggle generationToggle;
+
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
