80d1147abc7d842517f71b60935fdc6f0b1df729
NsStudioz <sneve2016@gmail.com>
2023-10-26 16:26:07 +0300

    Revert "Revert "new: UIController.cs = Added buttons listeners removal functions.""
    This reverts commit e241333c6fa5c710e734626609c02bddc908f8bf.

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,7 +6,8 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour
 {
     [Header("Main Elements")]
-    [SerializeField] private GameObject GamePanel;
+    [SerializeField] private GameObject PlayPanel;
+    [SerializeField] private GameObject MenuPanel;
 
     [Header("Maze Value Texts")]
     [SerializeField] private TMP_Text widthValueText;
@@ -18,19 +18,36 @@ public class UIController : MonoBehaviour
     [SerializeField] private Slider heightSilder;
 
     [Header("Buttons")]
-    [SerializeField] private Button generateMazeBtn;
+    [SerializeField] private Button playBtn;
     [SerializeField] private Button backBtn;
+    [SerializeField] private Button generateMazeBtn;
 
     [Header("Toggle")]
     [SerializeField] private Toggle generationToggle;
+    [SerializeField] private bool isQuickestGenerate; // testing the bool is in sync with toggle.
 
+    // Button Events:
+    public static event Action OnClickGenerateTheMazeButton;
+    public static event Action OnClickPlayButton;
+    public static event Action OnClickBackButton;
+    // Silders Events:
+    public static event Action<int> OnWidthValueChange;
+    public static event Action<int> OnHeightValueChange;
+    // Toggle Events:
+    public static event Action<bool> OnGenerationToggleChange;
 
-
-    void Start()
+    // Removing all UI listeners.
+    private void OnDestroy()
     {
-        
+        // Buttons:
+        playBtn.onClick.RemoveAllListeners();
+        backBtn.onClick.RemoveAllListeners();
+        generateMazeBtn.onClick.RemoveAllListeners();
+        // Sliders:
+        widthSilder.onValueChanged.RemoveAllListeners();
+        heightSilder.onValueChanged.RemoveAllListeners();
+        // Toggle:
+        generationToggle.onValueChanged.RemoveAllListeners();
     }
-
-
 
 }
