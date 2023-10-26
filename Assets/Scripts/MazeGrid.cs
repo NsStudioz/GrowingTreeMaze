df5641a3d6bf8eceaee71a7497b2dabf0802307c
NsStudioz <sneve2016@gmail.com>
2023-10-26 12:34:15 +0300

    fix: Fixed MazeGrid constructor.

--- Assets/Scripts/MazeGrid.cs
+++ Assets/Scripts/MazeGrid.cs
@@ -7,33 +7,10 @@ public class MazeGrid : MonoBehaviour
     public int width;
     public int height;
 
-    public MazeGrid(int width, int height, Transform parent, Node prefab)
+    public MazeGrid(int width, int height)
     {
         this.width = width;
         this.height = height;
-
-        // using x & z Axis's since we are generating a maze in 3D:
-        for (int x = 0; x < width; x++)
-            for (int z = 0; z < height; z++)
-            {
-                SpawnNewCellInstance(prefab, SetNodePosition(x, z));
-                SetNodeAsChildToParentGameObject(prefab, parent);
-            }
     }
 
-    private Node SpawnNewCellInstance(Node prefab, Vector3 cellPos)
-    {
-        return Instantiate(prefab, cellPos, Quaternion.identity);
-    }
-
-    private Vector3 SetNodePosition(int x, int z)
-    {
-        Vector3 nodePos = new Vector3(x, 0, z);
-        return nodePos;
-    }
-
-    private void SetNodeAsChildToParentGameObject(Node prefab, Transform parent)
-    {
-        prefab.transform.parent = parent;
-    }
 }
