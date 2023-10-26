eed749822c73ccb04be4cc97b25cc800200d5ab8
NsStudioz <sneve2016@gmail.com>
2023-10-26 12:41:53 +0300

    update: MazeGrid.cs = set fields to private.

--- Assets/Scripts/MazeGrid.cs
+++ Assets/Scripts/MazeGrid.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MazeGrid : MonoBehaviour
+public class MazeGrid
 {
-    public int width;
-    public int height;
+    private int width;
+    private int height;
 
     public MazeGrid(int width, int height)
     {
