afd076a337de1262cd297642bd0b79f566a429f5
NsStudioz <sneve2016@gmail.com>
2023-10-26 12:08:33 +0300

    new: node.cs = node walls feature added.

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Node : MonoBehaviour
 {
+    [SerializeField] private GameObject[] walls;
 
+    private void Awake()
+    {
+        InitializeNodeWalls();
+    }
+
+    private void InitializeNodeWalls()
+    {
+        walls = new GameObject[transform.childCount];
+
+        for (int i = 0; i < walls.Length; i++)
+            walls[i] = transform.GetChild(i).gameObject;
+    }
 
 }
