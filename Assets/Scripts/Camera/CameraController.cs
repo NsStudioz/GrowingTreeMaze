0d9d268b40f25996d6cf250e853903159b4fff29
NsStudioz <sneve2016@gmail.com>
2023-10-26 10:57:23 +0300

    new: CameraController.cs = added camera movement feature.

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Vector Elements")]
+    private Vector3 cameraPos;
 
-    // Update is called once per frame
+    [Header("Movement")]
+    [SerializeField] private float movementSpeed = 10f;
+
     void Update()
     {
-        
+        HandleMovement();
     }
+
+    private void HandleMovement()
+    {
+        transform.position = cameraPos;
+
+        HandleMovementVertical();
+        HandleMovementHorizontal();
+    }
+
+    private void HandleMovementHorizontal()
+    {
+        if (Input.GetKey(KeyCode.D))
+            cameraPos += transform.right * movementSpeed * Time.deltaTime;
+
+        else if (Input.GetKey(KeyCode.A))
+            cameraPos += transform.right * -movementSpeed * Time.deltaTime;
+    }
+
+    private void HandleMovementVertical()
+    {
+        if (Input.GetKey(KeyCode.W))
+            cameraPos += transform.forward * movementSpeed * Time.deltaTime;
+
+        else if (Input.GetKey(KeyCode.S))
+            cameraPos += transform.forward * -movementSpeed * Time.deltaTime;
+    }
+
 }
