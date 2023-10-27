a86f2aaf3ce79ef677042a5227a9dc7c26f6b399
NsStudioz <sneve2016@gmail.com>
2023-10-27 20:19:18 +0300

    new: GrowingTree.cs = new fields.

--- Assets/Scripts/GrowingTree.cs
+++ Assets/Scripts/GrowingTree.cs
@@ -1,8 +1,20 @@
+using PerfectMazeProject.Grid;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GrowingTree : MonoBehaviour
 {
+    [SerializeField] private MazeGrid grid;
+
+    [SerializeField] private List<Node> spawnedNodes = new List<Node>();
+    [SerializeField] private List<Node> nodes = new List<Node>();
+
+    [SerializeField] private int mazeWidth;
+    [SerializeField] private int mazeHeight;
+    [SerializeField] private bool isQuickestGenerate;
+    [SerializeField] private float generationSpeed = 0.01f;
+
+
 
 }
