29715af98ca8e49fb13b5bae7c5a56796c947e47
NsStudioz <sneve2016@gmail.com>
2023-10-27 10:19:03 +0300

    refactor: MazeGrid.cs = Made the grid creation method a getter instead and set in namespace.

--- Assets/Scripts/MazeGrid.cs
+++ Assets/Scripts/MazeGrid.cs
@@ -2,42 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MazeGrid : MonoBehaviour
+namespace PerfectMazeProject.Grid
 {
-
-    [SerializeField] private Node nodePrefab;
-
-    [SerializeField] private int gridWidth;
-    [SerializeField] private int gridHeight;
-
-    private void Start()
+    public class MazeGrid : MonoBehaviour
     {
-        DrawMazeGrid();
-    }
 
-    private void DrawMazeGrid()
-    {
-        for (int x = 0; x < gridWidth; x++)
-            for (int z = 0; z < gridHeight; z++)
-            {
-                Node newNode = SpawnNewCellInstance(nodePrefab, SetNodePosition(x, z));
-                SetNodeAsChildToParent(newNode, transform);
-            }
-    }
+        [SerializeField] private Node nodePrefab;
 
-    private Node SpawnNewCellInstance(Node prefab, Vector3 cellPos)
-    {
-        return Instantiate(prefab, cellPos, Quaternion.identity);
-    }
+        [SerializeField] private int gridWidth;
+        [SerializeField] private int gridHeight;
 
-    private Vector3 SetNodePosition(int x, int z)
-    {
-        Vector3 nodePos = new Vector3(x, 0, z);
-        return nodePos;
-    }
+        public List<Node> GetNodeGrid()
+        {
+            List<Node> nodeGridList = new List<Node>();
 
-    private void SetNodeAsChildToParent(Node prefab, Transform parent)
-    {
-        prefab.transform.parent = parent;
+            for (int x = 0; x < gridWidth; x++)
+                for (int z = 0; z < gridHeight; z++)
+                {
+                    Node newNode = SpawnNewCellInstance(nodePrefab, SetNodePosition(x, z));
+                    SetNodeAsChildToParent(newNode, transform);
+                    nodeGridList.Add(newNode);
+                }
+
+            return nodeGridList;
+        }
+
+        private Node SpawnNewCellInstance(Node prefab, Vector3 cellPos)
+        {
+            return Instantiate(prefab, cellPos, Quaternion.identity);
+        }
+
+        private Vector3 SetNodePosition(int x, int z)
+        {
+            Vector3 nodePos = new Vector3(x, 0, z);
+            return nodePos;
+        }
+
+        private void SetNodeAsChildToParent(Node prefab, Transform parent)
+        {
+            prefab.transform.parent = parent;
+        }
     }
 }
+
