89af26fcdaa3612ff5cffb137086be777865d01c
NsStudioz <sneve2016@gmail.com>
2023-10-28 23:11:29 +0300

    refactor: Node.cs = removed unused code blocks.

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Node : MonoBehaviour
@@ -8,52 +6,18 @@ public class Node : MonoBehaviour
 
     public bool IsVisited {  get; private set; }
 
-    [SerializeField] private bool visited = false;
+    #region PublicMethods:
 
-    public void SetNodeVisit()
-    {
-        visited = true;
-    }
-
-    public bool GetNodeVisit()
-    {
-        return visited;
-    }
-
-    private void Awake()
-    {
-        SetNodeInvisible();
-        ResetWallsState();
-    }
-
-    private void ResetWallsState()
+    public void ResetWallsState()
     {
         foreach (GameObject wall in walls)
-        {
             if (!wall.gameObject.activeInHierarchy)
                 wall.gameObject.SetActive(true);
-        }
     }
-
-    #region PublicMethods:
 
     public void SetNodeVisited()
     {
         IsVisited = true;
-    }
-    public void SetNodeUnvisited()
-    {
-        IsVisited = false;
-    }
-
-    public void SetNodeVisible()
-    {
-        gameObject.SetActive(true);
-    }
-
-    public void SetNodeInvisible()
-    {
-        gameObject.SetActive(false);
     }
 
     public void RemoveWall(int index)
